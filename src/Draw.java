15af3abd33be60c87d5f8c34f5c12778dff400d7
MohammedThan <MohammedTh.912@gmail.com>
2022-10-27 15:47:44 +0300

    new draw clas and Mydate class

---
+++ src/Draw.java
@@ -0,0 +1,16 @@
+import javafx.scene.shape.Line;
+
+public class Draw {
+
+    public static Line hLine(){    
+        Line  line = new Line(0,900,900,900);
+        return line;  
+    }
+
+    public static Line vLine(){    
+        Line  line = new Line(0,900,900,900);
+        return line;  
+    }
+
+    
+}
